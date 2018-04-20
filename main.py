517097b2e5a9c9dd18a2208e8c201755cd7787cf
Pontus Backman <ponbac@student.chalmers.se>
2018-04-20 12:46:59 +0200

    might have fixed socket error

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from time import sleep
 from random import randint
 from con_error import con_error
 from datetime import datetime
+import socket
 
 start_time = str(datetime.now())
 print("--Connection checker--")
 
-ip_list = ["216.58.192.142", "aftonbladet.se", "chalmers.se", "twitch.com", "sundsvall.se"]
+ip_list = ["216.58.192.142", "aftonbladet.se", "mixer.com", "op.gg", "sundsvall.se"]
 
 
 def internet_on(address):
@@ -16,10 +17,17 @@ def internet_on(address):
 
     for ip in ip_list:
         try:
-            request.urlopen("http://" + ip, timeout=3)
+            request.urlopen("http://" + ip, timeout=2)
             success = True
         except error.URLError as err:
-            print("Could not reach: " + ip)
+            print("URL that generated the error code: ", ip)
+            print("Error description:", err.reason)
+        except socket.timeout:
+            print("URL that generated the error code: ", ip)
+            print("Error description: No response.")
+        except socket.error:
+            print("URL that generated the error code: ", ip)
+            print("Error description: Socket error.")
 
     if success == False:
         con_error()
@@ -28,7 +36,7 @@ def internet_on(address):
 
 while True:
     internet_status = internet_on(ip_list)
-    print("Internet Connection: " + str(internet_status))
+    print("\nInternet Connection: " + str(internet_status))
     print("Time: " + str(datetime.now()))
     print("Last DC: " + str(con_error.last_dc))
     print("Total disconnects since " + start_time + ": " + str(con_error.total_dc) + "\n")
