9dfefa35563c52480540c392bf759e3ade696bbb
Pontus Backman <ponbac@student.chalmers.se>
2018-04-20 11:24:00 +0200

    added multiple ips to check

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ from datetime import datetime
 start_time = str(datetime.now())
 print("--Connection checker--")
 
+ip_list = ["216.58.192.142", "aftonbladet.se", "chalmers.se", "twitch.com", "sundsvall.se"]
 
-def internet_on():
-    try:
-        request.urlopen('http://216.58.192.142', timeout=1)
-        return True
-    except error.URLError as err:
+
+def internet_on(address):
+    success = False
+
+    for ip in ip_list:
+        try:
+            request.urlopen("http://" + ip, timeout=1)
+            success = True
+        except error.URLError as err:
+            print("Could not reach: " + ip)
+
+    if success == False:
         con_error()
-        return False
+
+    return success
 
 while True:
-    internet_status = internet_on()
+    internet_status = internet_on(ip_list)
     print("Internet Connection: " + str(internet_status))
     print("Time: " + str(datetime.now()))
     print("Last DC: " + str(con_error.last_dc))
